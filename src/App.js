bc2749587c5bdd75b9d601d9dbb02d7ed886dfd2
joshua-walters <87895876+jothwas@users.noreply.github.com>
2022-02-24 16:43:39 +0000

    add components, ToDoListGenerator functions and ToDoList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header.js";
+import ToDoGenerator from "./components/ToDoGenerator";
+import ToDoList from "./components/ToDoList";
 
 function App() {
-  return <div className="App"></div>;
+  const [list, setList] = useState([
+    { id: 1, task: "test task 1", done: false },
+    { id: 2, task: "test task 2", done: false },
+    { id: 3, task: "test task 3", done: false },
+  ]);
+  return (
+    <div className="App">
+      <Header />
+      <ToDoGenerator setList={setList} />
+      <ToDoList list={list} />
+    </div>
+  );
 }
 
 export default App;
